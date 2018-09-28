c5200468e27e68ec94441606d6e17a876cee7d2a
Angelvilad <angelvilad@hotmail.com>
2018-09-28 20:29:56 +0200

    created home/index.js, header-component

---
+++ src/pages/home/index.js
@@ -0,0 +1,6 @@
+import  { updateHeader } from 'components/header/header-component';
+// import { createArticles } from 'components/articles/articles-component';
+
+
+updateHeader({ title: 'VR Today!!'});
+//createArticles();
