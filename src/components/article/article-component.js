87b9f9e4d79bb4a3dfd8592341f485cace34009d
Angelvilad <angelvilad@hotmail.com>
2018-09-30 18:36:21 +0200

    Improve db arguments in function createArticle

--- src/components/article/article-component.js
+++ src/components/article/article-component.js
@@ -1,5 +1,5 @@
 export const createArticle = ({
-    title, author, mediaUrl, description, data_pub, comments, id
+    title, author, mediaUrl, text_article, data_pub, comments, id
 } = { title: 'No title', author: {user_name: 'No author', user_image: 'No Image'}}) => {
     const article = document.createElement('div');
     article.classList.add('article');
