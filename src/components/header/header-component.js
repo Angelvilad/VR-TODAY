a4e32540873f3a9405dbfb238e03c8d54b63e6e7
Angelvilad <angelvilad@hotmail.com>
2018-10-08 23:12:10 +0200

    Making responsive (mobile first)

--- src/components/header/header-component.js
+++ src/components/header/header-component.js
@@ -3,6 +3,14 @@ const updateTitle = (title) => {
     titleElement.innerHTML = title;
 };
 
+const hamburguerIcon = document.getElementById('hamburger-icon');
+const navMenu = document.getElementById('nav-sections');
+
+hamburguerIcon.addEventListener('click', () => {
+    navMenu.classList.toggle('nav-open');
+    
+});
+
 export const updateHeader = ({ title }) => {
     updateTitle(title);
 }
